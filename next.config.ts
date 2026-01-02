4715b28468464522a16ee0046d520b110e6cabfa
Nathan Tang <nathantang7744@gmail.com>
2026-01-02 19:05:25 +0800

    v2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/tools/buythedip",
+        destination:
+          "https://moonie-tool-buythedip-production-875a.up.railway.app/",
+      },
+      {
+        source: "/tools/buythedip/:path*",
+        destination:
+          "https://moonie-tool-buythedip-production-875a.up.railway.app/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
